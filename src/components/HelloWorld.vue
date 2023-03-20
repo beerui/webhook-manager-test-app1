19c28771547f24fcdacf2c80358843f8cde7a567
liuyongjie <liuyongjie@anthead.cn>
2023-03-20 17:41:11 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,8 +12,8 @@ defineProps({
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <a href="https://github.com/beerui/webhook-manager/" target="_blank" rel="noopener">webhook-manager</a> +
+      <a href="https://github.com/beerui/webhook-manager-test-app1" target="_blank" rel="noopener">test project</a>.
     </h3>
   </div>
 </template>
